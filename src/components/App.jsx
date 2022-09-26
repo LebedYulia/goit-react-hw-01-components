63fc29a86408c44da96c618d06efae5f3e07c6a0
Lebed Yulia <YuLebed@ukr.net>
2022-09-26 18:05:15 +0300

    feat: task 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
-import { Profile } from "./profile/Profile";
-import user from './profile/user.json'
+import { Profile } from './profile/Profile';
+import user from './profile/user.json';
+import { Statistics } from './statistics/Statistics';
+import data from './statistics/data.json';
+import { FriendList } from './friends/FriendList';
+import friends from './friends/friends.json';
 
 export const App = () => {
   return (
     <div>
-      <Profile 
-      username = {user.username} 
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
       />
-    </div>    
+
+      <Statistics title="Upload stats" stats={data} />
+
+      <FriendList items={friends} />
+    </div>
   );
 };
---
+++ src/components/friends/FriendList.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import { FriendListItem } from './FriendListItem';
+import css from './Friends.module.css';
+
+
+export const FriendList = ({ items }) => {
+  return (
+    <ul className={css.friends_list}>
+      {items.map(item => (        
+           <FriendListItem
+            key={item.id}
+            avatar={item.avatar}
+            name={item.name}
+            isOnline={item.isOnline}
+            />       
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
